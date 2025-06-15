d498a98349b0e8240f9b444a3899a3da24b4a58a
ykycha-kolot <yuriy@spiralyze.com>
2025-06-15 12:48:37 -0700

    frontend user count show from DB

--- website/index.js
+++ website/index.js
@@ -1,15 +1,84 @@
-const myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
 
-const raw = JSON.stringify({});
+const getUserCount = async () => {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-const requestOptions = {
-  method: "GET",
-  headers: myHeaders,
-  redirect: "follow"
+  try {
+    const response = await fetch(
+      "https://595io2smeg.execute-api.us-east-1.amazonaws.com/user-count",
+      {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+      }
+    );
+    const result_1 = await response.text();
+    return (userCountResponse = JSON.parse(result_1).count);
+  } catch (error) {
+    return console.error(error);
+  }
 };
 
-fetch("https://b2j38s5vqk.execute-api.us-east-1.amazonaws.com/visits", requestOptions)
-  .then((response) => response.text())
-  .then((result) => document.querySelector('.userCount').innerHTML = `<b>User Count:</b> ${JSON.parse(result).count}`)
-  .catch((error) => console.error(error));
+const setUserCount = async () => {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  try {
+    const response = await fetch(
+      "https://595io2smeg.execute-api.us-east-1.amazonaws.com/user-count",
+      {
+        method: "POST",
+        headers: myHeaders,
+        redirect: "follow",
+      }
+    );
+    const result_1 = await response.text();
+    return (userCountResponse = JSON.parse(result_1).count);
+  } catch (error) {
+    return console.error(error);
+  }
+};
+const updateDomValue = (value) => {
+  document.querySelector(
+    ".userCount"
+  ).innerHTML = `<b>User Count: ${value}</b>`;
+};
+const init = async () => {
+  const userBeenHere = getCookie("user");
+  let userCount = sessionStorage.getItem("userCount");
+  console.log(userCount);
+  if (userBeenHere) {
+    if (userCount === null) {
+      userCount = await getUserCount();
+      sessionStorage.setItem("userCount", userCount);
+    }
+  } else {
+    setCookie("user", "true", 365);
+    userCount = await setUserCount();
+  }
+  updateDomValue(userCount);
+};
+document.addEventListener("DOMContentLoaded", function () {
+  init();
+});
